8cfac6880687d525f710f0cc77049f1fe654242a
Fatou Diop SOLY <solyfatoudiop@gmail.com>
2023-12-22 12:59:12 +0100

    sowlymodiff

--- src/app/professeurs/notes/notes.component.spec.ts
+++ src/app/professeurs/notes/notes.component.spec.ts
@@ -1,15 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NotesComponent } from './notes.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('NotesComponent', () => {
   let component: NotesComponent;
   let fixture: ComponentFixture<NotesComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NotesComponent],
+      imports: [ReactiveFormsModule] // Importer ReactiveFormsModule pour les tests utilisant des formulaires réactifs
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NotesComponent]
-    });
     fixture = TestBed.createComponent(NotesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +22,23 @@ describe('NotesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should reset form and set editingIndex to null after saving note', () => {
+    // Simuler des données dans le formulaire
+    component.noteForm.get('semestre')?.setValue(1);
+    // ... Simuler d'autres champs du formulaire
+
+    // Simuler l'appel de la méthode saveNote
+    component.saveNote();
+
+    // Vérifier si le formulaire est réinitialisé après l'enregistrement
+    expect(component.noteForm.pristine).toBe(true);
+    expect(component.editingIndex).toBe(null);
+  });
+
+  // Vous pouvez écrire d'autres tests pour d'autres méthodes ou fonctionnalités du composant
 });
+
+
+
+
